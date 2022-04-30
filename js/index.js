29e314e7e450abe8e23a51d378b3558e1029d09a
ScorchedCoast <104597809+ScorchedCoast@users.noreply.github.com>
2022-04-30 09:43:18 -0400

    attempts at solving fooc

--- js/index.js
+++ js/index.js
@@ -14,3 +14,15 @@ $(document).ready(function() {
     }
 });
 */
+
+// Helper function
+let domReady = (cb) => {
+  document.readyState === 'interactive' || document.readyState === 'complete'
+    ? cb()
+    : document.addEventListener('DOMContentLoaded', cb);
+};
+
+domReady(() => {
+  // Display body when DOM is loaded
+  document.body.style.visibility = 'visible';
+});
